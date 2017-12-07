627a4985d35cb2594dae6790ea1e45324f79650f
jimistine <jrstine@me.com>
2017-12-07 17:52:49 -0500

    Spiraler working,
    about to bring it into the main scene.

--- Assets/Scripts/Spiraler.cs
+++ Assets/Scripts/Spiraler.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class Spiraler : MonoBehaviour {
 
+	public float circleSpeed = 1;
+	public float forwardSpeed = -1; // Assuming negative Z is towards the camera
+	public float circleSize = 1;
+	public float moveSpeed = 0.1f;
+ 
+	public float xPos;
+	public float yPos; 
+	public float zPos;	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		xPos = gameObject.transform.position.x;
+		yPos = gameObject.transform.position.y;
+		zPos = gameObject.transform.position.z;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
+		xPos = Mathf.Sin(Time.time * circleSpeed) * circleSize;
+		zPos = Mathf.Cos(Time.time * circleSpeed) * circleSize;
+//		zPos += forwardSpeed * Time.deltaTime;
+		
+		circleSize += moveSpeed;
+		
+		gameObject.transform.position = new Vector3(xPos, 0,zPos); 
 	}
 }
