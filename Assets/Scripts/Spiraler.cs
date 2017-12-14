f1e364c5e433c41cf752e0d388979d4e0c25d26d
jimistine <jrstine@me.com>
2017-12-14 15:11:14 -0500

    modified spiraler and fixed skybox

--- Assets/Scripts/Spiraler.cs
+++ Assets/Scripts/Spiraler.cs
@@ -1,22 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Spiraler : MonoBehaviour {
 
 	public float circleSpeed;
 	public float forwardSpeed;
 	public float circleSize;
-	public float circleGrowSpeed;
+	public float circleChangeSpeed;
  
 	public float xPos;
 	public float yPos; 
 	public float zPos;	
+	
+	public bool spiral_out;
+	public  bool spiral_in = true;
 	// Use this for initialization
 	void Start ()
 	{
 		xPos = gameObject.transform.position.x;
-		yPos = gameObject.transform.position.y;
+//		yPos = gameObject.transform.position.y;
 		zPos = gameObject.transform.position.z;
 	}
 	
@@ -27,9 +31,51 @@ public class Spiraler : MonoBehaviour {
 		xPos = Mathf.Sin(Time.time * circleSpeed) * circleSize;
 		zPos = Mathf.Cos(Time.time * circleSpeed) * circleSize;
 //		zPos += forwardSpeed * Time.deltaTime;
+
+		gameObject.transform.position = new Vector3(xPos, yPos, zPos);
+		circleSize += circleChangeSpeed;
+
+		if (circleSize < -50)
+		{
+			circleSize -= circleChangeSpeed;
+		}
 		
-		circleSize -= circleGrowSpeed;
-		
-		gameObject.transform.position = new Vector3(xPos, 0,zPos); 
+//		if (circleSize == -10)
+//		{
+//			circleSize = 10;
+//		}
+//		if (spiral_in)
+//		{
+//			circleSize += circleChangeSpeed;
+//		}
+//		else
+//			circleSize -= circleChangeSpeed;
+//
+//		if (circleSize == 0)
+//		{
+//			spiral_in = false;
+//			spiral_out = true;
+//		}
+//
+//		while (circleSize > -10)
+//		{
+//			SpiralIn();
+//		}
+//		while (circleSize <= -10)
+//		{
+//			SpiralOut();
+//		}
+//			
 	}
+//	
+//	private void SpiralIn()
+//	{
+//		circleSize += circleChangeSpeed;
+//	}
+//	
+//	private void SpiralOut()
+//	{
+//		circleSize -= circleChangeSpeed;
+//	}
 }
+
