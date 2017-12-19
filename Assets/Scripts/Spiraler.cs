4ae0f46eb03c00547dd5c16231f53b4618417ac9
Laura Chen <linkinmonkey@gmail.com>
2017-12-19 11:33:11 -0500

    Update

--- Assets/Scripts/Spiraler.cs
+++ Assets/Scripts/Spiraler.cs
@@ -16,66 +16,28 @@ public class Spiraler : MonoBehaviour {
 	
 	public bool spiral_out;
 	public  bool spiral_in = true;
-	// Use this for initialization
-	void Start ()
-	{
-		xPos = gameObject.transform.position.x;
-//		yPos = gameObject.transform.position.y;
-		zPos = gameObject.transform.position.z;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		
-		xPos = Mathf.Sin(Time.time * circleSpeed) * circleSize;
-		zPos = Mathf.Cos(Time.time * circleSpeed) * circleSize;
-//		zPos += forwardSpeed * Time.deltaTime;
+    // Use this for initialization
+    void Start()
+    {
+        xPos = gameObject.transform.position.x;
+        //		yPos = gameObject.transform.position.y;
+        zPos = gameObject.transform.position.z;
+    }
+    // Update is called once per frame
+    void Update()
+    {
 
-		gameObject.transform.position = new Vector3(xPos, yPos, zPos);
-		circleSize += circleChangeSpeed;
+        xPos = Mathf.Sin(Time.time * circleSpeed) * circleSize;
+        zPos = Mathf.Cos(Time.time * circleSpeed) * circleSize;
+        //		zPos += forwardSpeed * Time.deltaTime;
 
-		if (circleSize < -50)
-		{
-			circleSize -= circleChangeSpeed;
-		}
-		
-//		if (circleSize == -10)
-//		{
-//			circleSize = 10;
-//		}
-//		if (spiral_in)
-//		{
-//			circleSize += circleChangeSpeed;
-//		}
-//		else
-//			circleSize -= circleChangeSpeed;
-//
-//		if (circleSize == 0)
-//		{
-//			spiral_in = false;
-//			spiral_out = true;
-//		}
-//
-//		while (circleSize > -10)
-//		{
-//			SpiralIn();
-//		}
-//		while (circleSize <= -10)
-//		{
-//			SpiralOut();
-//		}
-//			
-	}
-//	
-//	private void SpiralIn()
-//	{
-//		circleSize += circleChangeSpeed;
-//	}
-//	
-//	private void SpiralOut()
-//	{
-//		circleSize -= circleChangeSpeed;
-//	}
+        gameObject.transform.position = new Vector3(xPos, yPos, zPos);
+        circleSize += circleChangeSpeed;
+
+        if (circleSize < -50)
+        {
+            circleSize -= circleChangeSpeed;
+        }
+    }	
 }
 
