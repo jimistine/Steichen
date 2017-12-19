2efd0ecf727a40e4c62a392948b1250afe0947a6
jimi <jrs825@nyu.edu>
2017-12-19 14:17:35 -0500

    Revert "Revert "Changes made to input catching""
    This reverts commit 9a43a71b81913b66bc58a1ed033a4c8e3b182bc0.

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+    public static GM Me;
+
+    public bool PlyerStop;
+
+    void OnAwake()
+    {
+        Me = this;
+    }
+}
--- Assets/Scripts/Spiraler.cs
+++ Assets/Scripts/Spiraler.cs
@@ -2,41 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Valve.VR.InteractionSystem;
 
 public class Spiraler : MonoBehaviour {
 
-	public float circleSpeed;
+    private SteamVR_TrackedController _controller;
+
+
+    public float circleSpeed;
 	public float forwardSpeed;
 	public float circleSize;
 	public float circleChangeSpeed;
  
 	public float xPos;
 	public float yPos; 
-	public float zPos;	
-	
-	public bool spiral_out;
-	public  bool spiral_in = true;
-    // Use this for initialization
+	public float zPos;
+
+    public bool spiral = true;
+
+
     void Start()
     {
         xPos = gameObject.transform.position.x;
-        //		yPos = gameObject.transform.position.y;
         zPos = gameObject.transform.position.z;
     }
-    // Update is called once per frame
+
     void Update()
     {
-
-        xPos = Mathf.Sin(Time.time * circleSpeed) * circleSize;
-        zPos = Mathf.Cos(Time.time * circleSpeed) * circleSize;
-        //		zPos += forwardSpeed * Time.deltaTime;
-
-        gameObject.transform.position = new Vector3(xPos, yPos, zPos);
-        circleSize += circleChangeSpeed;
-
-        if (circleSize < -50)
+        if (GM.Player.Stop)
         {
-            circleSize -= circleChangeSpeed;
+            xPos = Mathf.Sin(Time.time * circleSpeed) * circleSize;
+            zPos = Mathf.Cos(Time.time * circleSpeed) * circleSize;
+
+            gameObject.transform.position = new Vector3(xPos, yPos, zPos);
+            circleSize += circleChangeSpeed;
+
+            if (circleSize < -50)
+            {
+                circleSize -= circleChangeSpeed;
+            }
         }
     }	
 }
